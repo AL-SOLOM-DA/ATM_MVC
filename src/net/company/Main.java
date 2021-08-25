248ace62629f065ec2a4db4a67e51d5968359903
AL-SOLOM-DA <ecolog.comvgorle@mail.ru>
2021-08-25 18:44:00 +0300

    Initial commit

---
+++ src/net/company/Main.java
@@ -0,0 +1,13 @@
+package net.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    UserView userView = new UserView();
+	    SwingUtilities.invokeLater(()->{
+            userView.init();
+	    });
+    }
+}
